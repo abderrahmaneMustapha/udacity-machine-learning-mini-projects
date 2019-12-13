8e26357d6ba8dc8e2965b4f62e6c72fdb0e0b08e
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2019-12-13 17:45:27 +0100

    fix import train_test_split & lesson 2 mini project done

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -21,7 +21,22 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+def email_classify(features_train,  labels_train):
+    from sklearn.naive_bayes import GaussianNB
+    clf = GaussianNB()
 
+    fit_time = time()
+    clf.fit(features_train, labels_train)
+    print("time to train the classifier : ",round(time() - fit_time,3))
+
+    predict_time = time()
+    predict = clf.predict([[393939]])
+    print("time to make prediction : ",round(time() - predict_time,3))
+    
+    return clf.score(features_test, labels_test)
+
+
+print(email_classify(features_train,  labels_train))
 
 
 #########################################################
