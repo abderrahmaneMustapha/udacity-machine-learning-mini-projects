54ff0ad0f760efcd11e71e7502295f005dbbf51e
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-02-21 20:06:31 +0100

    lesson 8 done

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -2,18 +2,55 @@
 
 import pickle
 import sys
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    plt.scatter( salary, bonus )
+
+plt.xlabel("salary")
+plt.ylabel("bonus")
+plt.show()
 
 
+#find outliers 
+for key, value in data_dict.items():
+    if value['bonus'] == data.max():
+        #print (key)
+        pass
 
+data_dict.pop('TOTAL', 0)
+data = featureFormat(data_dict, features)
+
+plt.scatter(data[:,0], data[:,1])
+plt.xlabel("salary")
+plt.ylabel("bonus")
+plt.show()
+
+
+#find more outliers
+i=0
+while(i < 3):
+    deleted_key = "" 
+    for key, value in data_dict.items():
+      
+        if value['bonus'] == data.max():
+            print(i)
+            print(key)
+            deleted_key = key
+            print("delete this ",deleted_key)
+    data_dict.pop(deleted_key,0)
+    data = featureFormat(data_dict, features)
+    i+=1
+   
